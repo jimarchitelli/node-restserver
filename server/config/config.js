5c0bf2c9088c61e576ea9d83a792be45aaee6d36
Juan Marchitelli <jimarchitelli@gmail.com>
2019-07-07 09:54:59 -0300

    Mongo URI secured

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,6 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev')
    urlDB = 'mongodb://localhost:27017/cafe'
 else
-   urlDB = 'mongodb+srv://cafe:U0iOYoh1aXSnC2kN@cluster0-hfrgx.mongodb.net/cafe'
+   urlDB = process.env.MONGO_URI
 
 process.env.URL_DB = urlDB;
